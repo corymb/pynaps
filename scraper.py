a146c1d8fa7192ad4e5748c42648f420217236a4
davekcd <github@david-bush.co.uk>
2014-06-12 20:21:59 +0100

    Parsed test page with beautifulsoup.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,19 @@
 # -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+
 
 input_source = 'test_nap.html'
 with open(input_source, 'r') as page:
     data = page.read()
+
+
+def parse_naps(data=data):
+    soup = BeautifulSoup(data)
+    table = soup.find("div", {"id": "tipsTableOutput"})
+    rows = table.find_all("tr")
+    clean_rows = [[col.get_text().strip() for col in row.find_all(
+        "td")] for row in rows if row]
+    return [dict(zip(["naps", "time", "crs", "tipster", "spread"],
+        e)) for e in clean_rows if len(e) == 5]
+
+parse_naps()
